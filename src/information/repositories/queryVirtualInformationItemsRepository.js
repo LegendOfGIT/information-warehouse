1ec25448da04ee57de50b18d01fe17fc737baa2d
LegendOfGIT <marcel.schneider1@googlemail.com>
2023-01-23 22:56:48 +0100

    Update queryVirtualInformationItemsRepository.js

--- src/information/repositories/queryVirtualInformationItemsRepository.js
+++ src/information/repositories/queryVirtualInformationItemsRepository.js
@@ -2,7 +2,7 @@ const configuration = require('../../configuration/app-config')();
 const mongoClient = require('mongodb').MongoClient;
 
 module.exports = (query) => new Promise((resolve, reject) => {
-    mongoClient.connect(`mongodb://${configuration.host}:${configuration.port}/information-items`)
+    mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/information-items`)
         .then(database => {
             if(!database) {
                 resolve([]);
