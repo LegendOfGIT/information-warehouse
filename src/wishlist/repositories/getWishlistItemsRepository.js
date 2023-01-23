5474bf137b31cd9a70398baded00f66729548fbb
LegendOfGIT <marcel.schneider1@googlemail.com>
2023-01-23 23:00:12 +0100

    Update getWishlistItemsRepository.js

--- src/wishlist/repositories/getWishlistItemsRepository.js
+++ src/wishlist/repositories/getWishlistItemsRepository.js
@@ -2,7 +2,7 @@ const configuration = require('../../configuration/app-config')();
 const mongoClient = require('mongodb').MongoClient;
 
 module.exports = (userId) => new Promise((resolve, reject) => {
-    mongoClient.connect(`mongodb://${configuration.host}:${configuration.port}/wishlists`)
+    mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/wishlists`)
         .then(database => {
             if(!database) {
                 resolve([]);
