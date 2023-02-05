ecfa9ea897d1c1ef003e92f962d1e39d6bf46a8c
Marschner, Marcel <marcel.marschner@dfb.de>
2023-02-06 00:04:34 +0100

    - added category "women-jewelry"

--- src/configuration/observe-configuration.js
+++ src/configuration/observe-configuration.js
@@ -2,6 +2,7 @@ module.exports = {
     getCategoryIdByNavigationId: (navigationId) => {
         const mapping = {
             ELECTRONICS_AND_COMPUTERS_LARGE_APPLIANCES_FRIDGES_AND_FREEZERS: 'fridges-and-freezers',
+            FASHION_WOMEN_JEWELRY: 'women-jewelry',
             MULTIMEDIA_GAMES: 'multimedia-games',
             MULTIMEDIA_GAMES_NINTENDO_SWITCH: 'nintendo-switch-games',
             MULTIMEDIA_GAMES_PC: 'pc-games'
