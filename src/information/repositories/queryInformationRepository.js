949e4d8a5ff8b0c0f1b39b097ce09e0d613206ed
Marschner, Marcel <marcel.marschner@dfb.de>
2023-03-04 00:54:23 +0100

    -

--- src/information/repositories/queryInformationRepository.js
+++ src/information/repositories/queryInformationRepository.js
@@ -3,14 +3,13 @@ const mongoClient = require('mongodb').MongoClient;
 
 module.exports = (query, numberOfResults) => new Promise((resolve, reject) => {
 
-    console.log(numberOfResults);
     mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/information-items`)
         .then(database => {
             if(!database) {
                 resolve([]);
             }
 
-            database.db().collection('items').find(query).sort({ updatedOn: -1 }).limit(numberOfResults).toArray()
+            database.db().collection('items').find(query).sort({ updatedOn: -1 }).toArray()
                 .then((result) => {
                     resolve(result);
                 })
