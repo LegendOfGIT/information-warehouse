e54f94870f64fe4c571313c99c5964b04b3f3df6
Marcel Marschner <marcel.marschner@dfb.de>
2023-04-17 14:29:37 +0200

    - fixed update link bug

--- src/information/queryInformationItems.js
+++ src/information/queryInformationItems.js
@@ -11,9 +11,9 @@ module.exports = (query, randomItems, numberOfResults, page) => new Promise((res
     ).then((items) => {
         items = (items || []).filter(item => item['title-image']).map(item => {
             (item.providers || []).forEach(provider => {
-                provider.link = provider.link
-                    ? provider.link.replaceAll('\?.*', getCampaignParameterByUrl(provider.link))
-                    : '';
+                provider.link = -1 !== provider.link.indexOf('?')
+                        ? provider.link.replaceAll('\?.*', getCampaignParameterByUrl(provider.link))
+                        : provider.link + getCampaignParameterByUrl(provider.link);
             });
 
             return item;
