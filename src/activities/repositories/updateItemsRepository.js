422f83ad1b31246201fee55127b67851c2d9af45
Marcel Marschner <marcel.marschner@dfb.de>
2023-04-28 12:03:27 +0200

    - fixed update of non "mean" items

--- src/activities/repositories/updateItemsRepository.js
+++ src/activities/repositories/updateItemsRepository.js
@@ -4,26 +4,22 @@ const configuration = require('../../configuration/app-config')();
 const updateItem = (item) => {
     const url = `http://${configuration.services.satelliteController.host}:${configuration.services.satelliteController.port}/update-item`;
 
-    if (!(item.providers || []).filter(provider => provider.mean).length) {
-        (item.providers || []).forEach(provider => {
-            let itemId = provider.itemId || (item.mean || (item.asin ? `azo.${item.asin}` : ''));
-            itemId =
-                -1 !== (item.itemId || '').indexOf('otto.de') ? item.itemId.replace('otto.de-', 'otto.') :
-                -1 !== (item.itemId || '').indexOf('amazon.de') ? item.itemId.replace('azo.de-', 'otto.') :
-                itemId;
+    (item.providers || []).filter(provider => !provider.mean).forEach(provider => {
+        let itemId = provider.itemId || (item.mean || (item.asin ? `azo.${item.asin}` : ''));
+        itemId =
+            -1 !== (item.itemId || '').indexOf('otto.de') ? item.itemId.replace('otto.de-', 'otto.') :
+            -1 !== (item.itemId || '').indexOf('amazon.de') ? item.itemId.replace('azo.de-', 'otto.') :
+            itemId;
 
-            requestModule.post({
-                url,
-                json: {
-                    itemId,
-                    navigationPath: item.navigationPath }
-            }, () => {});
-        });
+        requestModule.post({
+            url,
+            json: {
+                itemId,
+                navigationPath: item.navigationPath }
+        }, () => {});
+    });
 
-        return;
-    }
-
-    (item.providers || []).forEach(provider => {
+    (item.providers || []).filter(provider => provider.mean).forEach(provider => {
         requestModule.post({
             url,
             json: { itemId: provider.mean, navigationPath: item.navigationPath }
