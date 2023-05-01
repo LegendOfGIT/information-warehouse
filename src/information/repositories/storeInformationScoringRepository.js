36e4c9efd02ae9098e2e00504cd5d3ddb2a2542d
mmarschner <marcel.marschner@adesso.de>
2023-05-01 22:50:32 +0200

    -

--- src/information/repositories/storeInformationScoringRepository.js
+++ src/information/repositories/storeInformationScoringRepository.js
@@ -42,6 +42,7 @@ module.exports = (informationItemScoring) => new Promise(async (resolve, reject)
     }
 
     increaseScoringValue(informationItemScoring, itemToScore, itemToScore);
+    await storeInformationRepository(itemToScore);
 
     const deepestNavigationId = itemToScore.navigationPath[itemToScore.navigationPath.length - 1];
     const furtherItemsToScore = await queryInformationRepository(
@@ -54,48 +55,10 @@ module.exports = (informationItemScoring) => new Promise(async (resolve, reject)
         20,
         1);
 
-    furtherItemsToScore.forEach(item => {
+    for (let item of furtherItemsToScore) {
         increaseScoringValue(informationItemScoring, itemToScore, item);
-        console.log(item.scoring);
-    })
-
+        await storeInformationRepository(item);
+    }
 
     resolve();
-
-
-
-    /*mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/information-items`)
-        .then((database) => {
-            const collection = database.db().collection('items');
-
-            collection.findOne({ itemId: informationItemScoring.itemId })
-                .then(informationItem => {
-
-                    let { scoring } = informationItem;
-                    scoring = scoring || {};
-
-                    scoring[informationItemScoring.searchProfileId] = scoring[informationItemScoring.searchProfileId] || 0;
-                    scoring[informationItemScoring.searchProfileId] += informationItemScoring.scoring;
-                    informationItem.scoring = scoring;
-
-                    collection.replaceOne(
-                        { itemId: informationItem.itemId },
-                        informationItem,
-                        { upsert: true }
-                    )
-                        .then(response => {
-                            resolve(response);
-                        })
-                        .catch(error => {
-                            reject(error);
-                        })
-                        .finally(() => {
-                            database.close();
-                        });
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        })
-        .catch(error => { reject(error); });*/
 });
