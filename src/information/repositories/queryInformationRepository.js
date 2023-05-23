f856e5008b03b9a18bb9d2e3c12ddeca45534bde
Marcel Marschner <marcel.marschner@dfb.de>
2023-05-23 23:19:46 +0200

    - search terms can now be searched without special characters

--- src/information/repositories/queryInformationRepository.js
+++ src/information/repositories/queryInformationRepository.js
@@ -17,7 +17,7 @@ module.exports = (query, hashtag, randomItems, numberOfResults, page) => new Pro
             sort.updatedOn = -1;
 
             if (query.title) {
-                query.titleWithoutSpecials = new RegExp(`.*${query.title}.*`);
+                query.titleWithoutSpecials = new RegExp(`.*${query.title}.*`, 'i');
                 delete query.title;
             }
 
