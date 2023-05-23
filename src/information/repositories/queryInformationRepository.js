627f903615b93f37f5663bdbfd40b5c9b3c11e41
Marcel Marschner <marcel.marschner@dfb.de>
2023-05-24 00:04:00 +0200

    - roll back

--- src/information/repositories/queryInformationRepository.js
+++ src/information/repositories/queryInformationRepository.js
@@ -16,37 +16,7 @@ module.exports = (query, hashtag, randomItems, numberOfResults, page) => new Pro
             sort.numberOfRatings = -1;
             sort.updatedOn = -1;
 
-            if (query.title) {
-                query.titleWithoutSpecials = new RegExp(`.*${query.title}.*`, 'i');
-                delete query.title;
-            }
-
-
             const queryParts = [
-                {
-                    $addFields: {
-                        titleWithoutSpecials: {
-                            $reduce: {
-                                input: {
-                                    $split: ["$title", /[/,|']/]
-                                },
-                                initialValue: "",
-                                in: {
-                                    $concat: [
-                                        "$$value",
-                                        {
-                                            $cond: {
-                                                if: { $eq: ["$$value", ""] },
-                                                then: "$$this",
-                                                else: ""
-                                            }
-                                        }
-                                    ]
-                                }
-                            }
-                        }
-                    }
-                },
                 { $match: { ...query, ...priceCheck } },
                 { $sort: sort}
             ];
