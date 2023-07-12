d2b6d8502c4c750e36658e9be33a9bf710da6903
Marcel Marschner <marcel.marschner@dfb.de>
2023-07-12 13:41:40 +0200

    - query for pages count is now unified with general query composition

--- src/information/repositories/queryInformationRepository.js
+++ src/information/repositories/queryInformationRepository.js
@@ -1,60 +1,22 @@
 const configuration = require('../../configuration/app-config')();
-const filterConfiguration = require('../../configuration/filter-configuration');
+const queryPartsResolver = require("./queryPartsResolver");
 const mongoClient = require('mongodb').MongoClient;
 
 module.exports = (query, hashtag, randomItems, numberOfResults, page, filterIds = []) => new Promise((resolve, reject) => {
-
-    const getFilterQuery = (filterIds) => {
-        const filterProperties = filterConfiguration.getFilterPropertiesByFilterIds(filterIds);
-        if (0 === Object.keys(filterProperties).length) {
-            return {};
-        }
-
-        const queries = [];
-        Object.values(filterProperties).forEach(filterProperty => {
-            const subQueries = [];
-            filterProperty.forEach(propertyFilter => {
-                const query = { };
-                query[`filterInformation.${propertyFilter.property}`] = RegExp(propertyFilter.value, 'i');
-                subQueries.push(query);
-            });
-            queries.push({ $or: subQueries });
-        });
-
-        console.log(queries);
-        return { $and: queries };
-    };
-
     mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/information-items`)
         .then(database => {
             if (!database) {
                 resolve([]);
             }
 
-            const priceCheck = numberOfResults > 1 ? { hasPriceInformation: { $in: [true, null] } } : null;
+            const queryParts = queryPartsResolver(query, numberOfResults, filterIds);
+
             const sort = {};
             sort['scoring.' + (hashtag || 'noprofile')] = -1;
             sort.ratingInPercent = -1;
             sort.numberOfRatings = -1;
             sort.updatedOn = -1;
 
-            if (query.title) {
-                query.titleWithoutSpecials = new RegExp(`.*${query.title.split(/'|´|`/).join('').replaceAll('á', 'a').replaceAll('é', 'e').replaceAll('ó', 'o')}.*`, 'i');
-                delete query.title;
-            }
-
-            const queryParts = [
-                { $addFields: { titleWithoutSpecials: { $replaceAll: { input: "$title", find: "'", replacement: '' }}}}
-            ];
-
-            const specialCharReplacements = {
-                '`': '', '´': '', ':': '',
-                'á': 'a', 'é': 'e', 'ó': 'o'
-            };
-            Object.keys(specialCharReplacements).forEach(
-                key => queryParts.push({ $addFields: { titleWithoutSpecials: { $replaceAll: { input: "$titleWithoutSpecials", find: key, replacement: specialCharReplacements[key] }}}}));
-
-            queryParts.push({ $match: { ...query, ...priceCheck, ...getFilterQuery(filterIds) } });
             queryParts.push({ $sort: sort });
 
             if ((/true/i).test(randomItems)) {
