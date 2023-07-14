a2b9e79bf874cb57ceaf415c171ab795e7b585c4
Marcel Marschner <marcel.marschner@dfb.de>
2023-07-14 11:12:45 +0200

    - added color filters

--- src/configuration/filter-configuration.js
+++ src/configuration/filter-configuration.js
@@ -1,8 +1,22 @@
 const FILTER_MAPPING = {
     1000000: { property: 'providers.itemId', value: 'dress-for-less' },
     1000001: { property: 'providers.itemId', value: 'biggreensmile' },
-    1000004: { property: 'filterInformation.color', value: 'red' },
+
+    1000014: { property: 'filterInformation.color', value: 'black' },
+    1000008: { property: 'filterInformation.color', value: 'blue' },
+    1000009: { property: 'filterInformation.color', value: 'brown' },
+    1000006: { property: 'filterInformation.color', value: 'darkblue' },
+    1000007: { property: 'filterInformation.color', value: 'darkred' },
+    1000011: { property: 'filterInformation.color', value: 'gold' },
     1000005: { property: 'filterInformation.color', value: 'gray' },
+    1000012: { property: 'filterInformation.color', value: 'green' },
+    1000013: { property: 'filterInformation.color', value: 'pink' },
+    1000017: { property: 'filterInformation.color', value: 'purple' },
+    1000015: { property: 'filterInformation.color', value: 'silver' },
+    1000004: { property: 'filterInformation.color', value: 'red' },
+    1000016: { property: 'filterInformation.color', value: 'white' },
+    1000010: { property: 'filterInformation.color', value: 'yellow' },
+
     1000002: { property: 'filterInformation.fit', value: 'flare fit' },
     1000003: { property: 'filterInformation.fit', value: 'skinny fit' }
 };
