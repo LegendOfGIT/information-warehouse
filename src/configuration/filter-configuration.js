2b719ba123357d9865f8ff28aeacde45755d6703
Marcel Marschner <marcel.marschner@dfb.de>
2023-07-17 12:04:49 +0200

    - added product-type filter

--- src/configuration/filter-configuration.js
+++ src/configuration/filter-configuration.js
@@ -34,6 +34,7 @@ const FILTER_MAPPING = {
     1000048: { property: 'providers.itemId', value: 'topparfuemerie' },
     1000049: { property: 'providers.itemId', value: 'waschbaer' },
     1000050: { property: 'providers.itemId', value: 'whitecollection' },
+    1000052: { property: 'providers.itemId', value: 'messmer' },
 
     1000014: { property: 'filterInformation.color', value: 'black' },
     1000008: { property: 'filterInformation.color', value: 'blue' },
