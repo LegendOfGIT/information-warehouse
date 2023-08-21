d02882c8335a85df08fc85f537a1a0317fa4678e
Marcel Marschner <marcel.marschner@dfb.de>
2023-08-21 12:16:56 +0200

    - introduced first cache (for available filters 1 hour)

--- src/filters/getAvailableFilters.js
+++ src/filters/getAvailableFilters.js
@@ -1,12 +1,29 @@
 const queryInformation = require('../information/repositories/queryInformationRepository');
 const filterConfiguration = require('../configuration/filter-configuration');
+const cache = require('../cache/cache');
 
-module.exports = (parameters) => new Promise(async (resolve, reject) => {
+const getCacheKey = (parameters) => {
     const {
         priceFrom,
         priceTo,
         query
     } = parameters;
+
+    return [query.title, query.navigationPath, priceFrom, priceTo].join('||');
+};
+
+module.exports = (parameters) => new Promise(async (resolve) => {
+    const {
+        priceFrom,
+        priceTo,
+        query
+    } = parameters;
+
+    const cacheKey = getCacheKey(parameters);
+    if (cache.has(cacheKey)) {
+        resolve(cache.get(cacheKey));
+        return;
+    }
 
     const filterPromises = Object.keys(filterConfiguration.FILTER_MAPPING)
         .map(filterId => queryInformation({ query, priceFrom, priceTo, filterIds: [filterId], numberOfResults: 1 }));
@@ -14,5 +31,7 @@ module.exports = (parameters) => new Promise(async (resolve, reject) => {
         .map(items => { return items.length > 0 ? { filterId: items[0].filterId } : undefined; });
 
     availableFilters = availableFilters.filter(f => f);
+
+    cache.set(cacheKey, availableFilters, 60);
     resolve(availableFilters);
 });
