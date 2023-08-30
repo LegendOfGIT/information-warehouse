d41598308fea721a0aba161c3433d94a5d1264c4
Marcel Marschner <marcel.marschner@dfb.de>
2023-08-30 22:30:10 +0200

    - visitedCategories activities older than 1 month are now removed, when a category was picked

--- src/activities/repositories/storeActivityVisitedCategoryRepository.js
+++ src/activities/repositories/storeActivityVisitedCategoryRepository.js
@@ -38,6 +38,12 @@ module.exports = (hashtag, navigationId, maximumNumberOfResults, isBotRequest) =
     mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/activities`)
         .then((database) => {
             const collection = database.db().collection('visitedCategories');
+
+            const date2MonthAgo = new Date();
+            date2MonthAgo.setMonth(date2MonthAgo.getMonth() - 1);
+
+            collection.removeMany({ activityOn: { $lt: date2MonthAgo }}).then(() => {}).catch(() => {});
+
             collection.insertOne({
                 categoryId,
                 hashtag,
