502b576262c7e1c1d2143294baf71757c003d668
Marcel Marschner <marcel.marschner@dfb.de>
2023-09-20 13:09:16 +0200

    - added new shop

--- src/configuration/filter-configuration.js
+++ src/configuration/filter-configuration.js
@@ -1,4 +1,5 @@
 const FILTER_MAPPING = {
+    1000176: { property: 'providers.itemId', value: 'amd-moebel' },
     1000144: { property: 'providers.itemId', value: 'artgerecht' },
     1000000: { property: 'providers.itemId', value: 'dress-for-less' },
     1000001: { property: 'providers.itemId', value: 'biggreensmile' },
