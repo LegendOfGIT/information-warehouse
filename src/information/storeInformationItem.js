75dc7a85e34b9c2bede07a77b2b9a07129768eef
Marcel Marschner <marcel.marschner@dfb.de>
2024-01-11 16:31:33 +0100

    - introduced price history

--- src/information/storeInformationItem.js
+++ src/information/storeInformationItem.js
@@ -7,6 +7,29 @@ const getItemIdFromInformationItem = (item) => {
     return `${(item.navigationPath  || []).join('-')}-${item.gtin || item.asin || item.ean || item.mean}`;
 };
 
+const updatePriceHistoryOfItem = (item) => {
+    if (!item?.providers) {
+        return;
+    }
+
+    const latestLowestPrice = Math.min((item.providers || []).map(provider => provider['price-current'] || 0));
+    if (!latestLowestPrice) {
+        return;
+    }
+
+    const todaysDate = new Date().getDate();
+    const priceHistory = (item.priceHistory || []);
+    let todaysHistoryEntry = priceHistory.find(historyEntry => todaysDate === historyEntry.date.getDate());
+    if (!todaysHistoryEntry) {
+        todaysHistoryEntry = { date: new Date() };
+        priceHistory.push(todaysHistoryEntry);
+    }
+
+    todaysHistoryEntry.lowestCurrentPrice = latestLowestPrice;
+
+    item.priceHistory = priceHistory;
+}
+
 const itemToStoreFromScrapedItem = (storedItem, scrapedItem, overrideProviders) => {
     const providerSpecificProperties = [
         'amountInStock',
@@ -63,6 +86,7 @@ const itemToStoreFromScrapedItem = (storedItem, scrapedItem, overrideProviders) 
 
     itemToStore.hasPriceInformation = providers.filter(provider => provider['price-initial'] || provider['price-current']).length > 0;
     itemToStore.isInStock = providers.filter(provider => undefined === provider.amountInStock || provider.amountInStock > 0).length > 0;
+    updatePriceHistoryOfItem(itemToStore);
 
     const providersWithBothPrices = providers.filter(provider => provider['price-initial'] && provider['price-current']);
     const reductions = providersWithBothPrices.map(p => (100 - (p['price-current'] * 100 / p['price-initial'])));
