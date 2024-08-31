801574963467412e0421414e934093deac6194bd
Marcel Marschner <marcel.marschner@dfb.de>
2024-08-31 23:41:23 +0200

    - added route to delete a wishlist - added route to get wishlists

--- src/wishlist/repositories/getWishlistsRepository.js
+++ src/wishlist/repositories/getWishlistsRepository.js
@@ -1,7 +1,7 @@
 const configuration = require('../../configuration/app-config')();
 const mongoClient = require('mongodb').MongoClient;
 
-module.exports = (userId) => new Promise((resolve, reject) => {
+module.exports = ({userId}) => new Promise((resolve, reject) => {
     mongoClient.connect(`mongodb://${configuration.database.host}:${configuration.database.port}/wishlists`)
         .then(database => {
             if(!database) {
